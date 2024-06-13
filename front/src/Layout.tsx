44e1ff8dc31192445d092ffce52bbd2abf443529
Mohammed AlSammak <msk170a@gmail.com>
2024-06-13 21:42:26 +0300

    ui

--- front/src/Layout.tsx
+++ front/src/Layout.tsx
@@ -1,19 +1,19 @@
 import { JSXElement } from "solid-js";
 
 type LayoutProps = {
-    children: JSXElement
+  children: JSXElement
 }
 
 export default function Layout(props: LayoutProps): JSXElement {
-    return (
-        <>
-        <header class=" bg-slate-950">
-            This is header
-        </header>
-        <main>
-            {props.children}
-        </main>
-        <footer>this is footer</footer>
-        </>
-    )
+  return (
+    <>
+      <header class="pt-5 pb-5">
+        This is header
+      </header>
+      <main>
+        {props.children}
+      </main>
+      <footer>this is footer</footer>
+    </>
+  )
 }
--- front/src/pages/home/index.tsx
+++ front/src/pages/home/index.tsx
@@ -1,10 +1,15 @@
 import { JSXElement } from "solid-js";
 import Layout from "../../Layout";
+import { Button } from "../../components/ui/button";
 
 export default function HomePage(): JSXElement {
   return (
     <Layout>
-      <h1>Home Page</h1>
+      <h1 class="p-5">Home Page</h1>
+      <Button>Click!</Button>
+      <Button variant={"outline"}>outline</Button>
+      <Button variant={"ghost"}>ghost</Button>
+      <Button variant={"secondary"}>DO NOT</Button>
     </Layout>
   );
 }

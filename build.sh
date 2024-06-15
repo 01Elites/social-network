dadb8a21a15511563b12951a2cad945a257ddab4
amali <e.amjad2010@gmail.com>
2024-06-15 16:18:26 +0300

    chore: Add build script for Docker containers and Go application

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Make this script executable
+chmod +x $0
+
+# Navigate to the directory containing the Docker Compose file
+cd backend/database
+
+# Start Docker containers
+sudo docker-compose up --build -d
+
+# Navigate back to the project root directory to run the Go application
+cd ../../
+
+# Run Go application
+go run cmd/socialNetwork/main.go 
+
+# When the Go application exits, stop Docker containers
+cd backend/database
+sudo docker-compose down

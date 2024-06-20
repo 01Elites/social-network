c5715069981e141746483f588b5322b5c25271c7
Mohammed AlSammak <msk170a@gmail.com>
2024-06-21 02:45:13 +0300

    lazy fixing icons padding

--- front/src/components/core/sidebar/index.tsx
+++ front/src/components/core/sidebar/index.tsx
@@ -1,14 +1,15 @@
 import { JSXElement } from "solid-js";
-// import SettingsIcon from '@mui/icons-material/Settings';
-import SettingsIcon from "@suid/icons-material/Settings";
-import AppsIcon from '@suid/icons-material/Apps';
+// import SettingsIcon from "@suid/icons-material/Settings";
+// import AppsIcon from '@suid/icons-material/Apps';
 // import PeopleIcon from '@suid/icons-material/People';
 // import GroupsIcon from '@suid/icons-material/Groups';
-import FlagIcon from '@suid/icons-material/Flag';
+// import FlagIcon from '@suid/icons-material/Flag';
 import './styles.css'
 import Two_Persons_Icon from '../../../components/ui/icons/two_person/two_person';
 import Group_Icon from "../../../components/ui/icons/group/group_icon";
 import Settings_Icon from "../../../components/ui/icons/settings/settings";
+import Apps_Icon from "../../../components/ui/icons/apps/apps_icon";
+import Flag_Icon from "../../../components/ui/icons/flag/flag_icon";
 
 type SidebarProps = {
   children: JSXElement;
@@ -17,20 +18,17 @@ type SidebarProps = {
 export default function SideBar(props: SidebarProps) {
   return (
     <div class="grid grid-cols-[max-content_1fr] SideHeight">
-      <aside class="slef-start sticky top-0 flex flex-col justify-between ml-9 mt-9">
+      <aside class="slef-start sticky top-0 flex flex-col justify-between p-5">
         {/* This div is for Navigation */}
         <div class="flex flex-col gap-10">
-          <AppsIcon class="cursor-pointer" onClick={() => { console.log("apps clicked") }} fontSize="large" />
-          {/* <PeopleIcon class="cursor-pointer" onClick={() => { console.log("two person clicked") }} fontSize="large" /> */}
-          <Two_Persons_Icon class="w-7 h-7 cursor-pointer" onClick={() => { console.log("two person clicked") }} />
-          {/* <GroupsIcon class="cursor-pointer" onClick={() => { console.log("group clicked") }} fontSize="large" /> */}
-          <Group_Icon class="cursor-pointer w-8 h-8" onClick={() => { console.log("group clicked") }} />
-          <FlagIcon class="cursor-pointer" onClick={() => { console.log("flag clicked") }} fontSize="large" />
+          <Apps_Icon class="cursor-pointer w-8 h-8 self-center" onClick={() => { console.log("apps clicked") }} />
+          <Two_Persons_Icon class="w-7 h-7 cursor-pointer self-center" onClick={() => { console.log("two person clicked") }} />
+          <Group_Icon class="cursor-pointer w-8 h-8 self-center" onClick={() => { console.log("group clicked") }} />
+          <Flag_Icon class="cursor-pointer w-8 h-8 flex self-center" onClick={() => { console.log("flag clicked") }} />
         </div>
 
         {/* This one will have the settings */}
-        {/* <SettingsIcon class="cursor-pointer" onClick={() => { console.log("settings clicked") }} fontSize="large" /> */}
-        <Settings_Icon class="w-9 h-9 cursor-pointer" onClick={() => { console.log("settings clicked") }} />
+        <Settings_Icon class="w-9 h-9 cursor-pointer self-center" onClick={() => { console.log("settings clicked") }} />
       </aside>
 
       {/* Children will be the contents */}

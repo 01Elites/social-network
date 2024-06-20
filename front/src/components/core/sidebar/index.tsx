7e1e7d291c70fc1c99721adb519e3e83319f692f
Mohammed AlSammak <msk170a@gmail.com>
2024-06-20 23:48:01 +0300

    icons

--- front/src/components/core/sidebar/index.tsx
+++ front/src/components/core/sidebar/index.tsx
@@ -1,12 +1,14 @@
 import { JSXElement } from "solid-js";
 // import SettingsIcon from '@mui/icons-material/Settings';
-import Home from "@suid/icons-material/Home";
 import SettingsIcon from "@suid/icons-material/Settings";
 import AppsIcon from '@suid/icons-material/Apps';
-import PeopleIcon from '@suid/icons-material/People';
-import GroupsIcon from '@suid/icons-material/Groups';
-import PersonIcon from '@suid/icons-material/Person';
+// import PeopleIcon from '@suid/icons-material/People';
+// import GroupsIcon from '@suid/icons-material/Groups';
+import FlagIcon from '@suid/icons-material/Flag';
 import './styles.css'
+import Two_Persons_Icon from '../../../components/ui/icons/two_person/two_person';
+import Group_Icon from "../../../components/ui/icons/group/group_icon";
+import Settings_Icon from "../../../components/ui/icons/settings/settings";
 
 type SidebarProps = {
   children: JSXElement;
@@ -18,15 +20,17 @@ export default function SideBar(props: SidebarProps) {
       <aside class="slef-start sticky top-0 flex flex-col justify-between ml-9 mt-9">
         {/* This div is for Navigation */}
         <div class="flex flex-col gap-10">
-          <Home fontSize="large" />
-          <AppsIcon fontSize="large" />
-          <PersonIcon fontSize="large" />
-          <PeopleIcon fontSize="large" />
-          <GroupsIcon fontSize="large" />
+          <AppsIcon class="cursor-pointer" onClick={() => { console.log("apps clicked") }} fontSize="large" />
+          {/* <PeopleIcon class="cursor-pointer" onClick={() => { console.log("two person clicked") }} fontSize="large" /> */}
+          <Two_Persons_Icon class="w-7 h-7 cursor-pointer" onClick={() => { console.log("two person clicked") }} />
+          {/* <GroupsIcon class="cursor-pointer" onClick={() => { console.log("group clicked") }} fontSize="large" /> */}
+          <Group_Icon class="cursor-pointer w-8 h-8" onClick={() => { console.log("group clicked") }} />
+          <FlagIcon class="cursor-pointer" onClick={() => { console.log("flag clicked") }} fontSize="large" />
         </div>
 
         {/* This one will have the settings */}
-        <SettingsIcon fontSize="large" />
+        {/* <SettingsIcon class="cursor-pointer" onClick={() => { console.log("settings clicked") }} fontSize="large" /> */}
+        <Settings_Icon class="w-9 h-9 cursor-pointer" onClick={() => { console.log("settings clicked") }} />
       </aside>
 
       {/* Children will be the contents */}
---
+++ front/src/components/ui/icons/group/group_icon.tsx
@@ -0,0 +1,17 @@
+import group from './group.svg'
+
+interface inputs {
+  class?: string;
+  onClick?: () => void;
+}
+
+export default function Group_Icon(props: inputs) {
+  return (
+    <img
+      src={group}
+      alt='Group'
+      class={props ? props.class : ''}
+      onClick={props ? props.onClick : () => { }}
+    />
+  )
+}

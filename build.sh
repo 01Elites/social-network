ac20dc3af44dc41e341ca34283ce8363ecbbdf78
amali <e.amjad2010@gmail.com>
2024-06-23 11:54:07 +0300

    chore: Update build script to remove unnecessary code and simplify Docker container startup

--- build.sh
+++ build.sh
@@ -9,31 +9,10 @@ trap cleanup EXIT
 # Function to stop Docker containers
 cleanup() {
     echo "Stopping Docker containers..."
-    cd internal/database || { echo "Directory internal/database not found"; exit 1; }
     sudo docker-compose down
     echo "Docker containers stopped."
 }
 
-# Navigate to the directory containing the Docker Compose file
-cd internal/database || { echo "Directory internal/database not found"; exit 1; }
-
 # Start Docker containers
 echo "Starting Docker containers..."
-sudo docker-compose up -d
-if [ $? -ne 0 ]; then
-    echo "Failed to start Docker containers"
-    exit 1
-fi
-echo "Docker containers started."
-
-# Navigate back to the project root directory to run the Go application
-cd ../../ || { echo "Failed to navigate to project root directory"; exit 1; }
-
-# Run Go application
-echo "Running Go application..."
-go run cmd/socialNetwork/main.go
-if [ $? -ne 0 ]; then
-    echo "Go application exited with an error"
-    exit 1
-fi
-echo "Go application exited successfully."
+sudo docker-compose up 

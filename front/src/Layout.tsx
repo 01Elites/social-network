0b8deb95ab8b52214b9a49ab0022400a7cd98317
ExTBH <natheer@extbh.dev>
2024-06-24 21:48:45 +0300

    new home structure

--- front/src/Layout.tsx
+++ front/src/Layout.tsx
@@ -9,11 +9,18 @@ type LayoutProps = {
 
 export default function Layout(props: LayoutProps): JSXElement {
   return (
-    <>
-      <Navbar />
-      <SideBar>
+    <div class='flex h-screen w-full flex-col bg-red-400'>
+      <div class='fixed flex h-[40px] w-full bg-green-400'>THIS IS NAV</div>
+
+      <div class='flex grow bg-blue-300'>
+        <div class='fixed h-full w-10 bg-neutral-600'>THIS IS SIDEBAR</div>
+
         <main>{props.children}</main>
-      </SideBar>
-    </>
+      </div>
+
+      {/* <Navbar />
+      
+      <SideBar></SideBar> */}
+    </div>
   );
 }

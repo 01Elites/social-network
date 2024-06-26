5e818caa0a9e3241bf72f4b0d8efb3bde11d1fa0
Mohammed AlSammak <msk170a@gmail.com>
2024-06-26 17:47:31 +0300

    checking and installing missing dependencies (postgres and caddy)

--- build.sh
+++ build.sh
@@ -8,11 +8,13 @@ trap cleanup EXIT
 
 # Function to stop Docker containers
 cleanup() {
-    echo "Stopping Docker containers..."
-    sudo docker-compose down
-    echo "Docker containers stopped."
+	echo "Stopping Docker containers..."
+	docker-compose down
+	echo "Docker containers stopped."
 }
 
 # Start Docker containers
 echo "Starting Docker containers..."
-sudo docker-compose up 
+docker pull postgres
+docker pull caddy
+docker-compose up

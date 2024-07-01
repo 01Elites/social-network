5478b4ce443c64f3113887abc6b25e035f19ff11
emahfoodh <eman.mjm@gmail.com>
2024-07-01 19:52:16 +0300

    update

--- internal/views/middleware/middleware.go
+++ internal/views/middleware/middleware.go
@@ -14,6 +14,7 @@ const UserIDKey contextKey = "userID"
 
 func ValidateSessionMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 		userID, err := ValidateSession(w, r)
 		if err != nil {
 			http.Error(w, "Invalid session token", http.StatusUnauthorized)

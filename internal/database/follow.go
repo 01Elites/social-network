5dc348207c670779c9654de57df3b1f1e48afe7c
amali <e.amjad2010@gmail.com>
2024-07-02 17:35:12 +0300

    add IsFollowing func

--- internal/database/follow.go
+++ internal/database/follow.go
@@ -6,7 +6,6 @@ import (
 	"social-network/internal/models"
 )
 
-
 func GetUsersFollowingByID(userID string) (map[string]bool, error) {
 	Following := make(map[string]bool)
 	query := `SELECT followed_id FROM follower WHERE follower_id = $1`
@@ -26,7 +25,7 @@ func GetUsersFollowingByID(userID string) (map[string]bool, error) {
 	return Following, nil
 }
 
-func CreateFollowRequest(request models.Request)(int, error){
+func CreateFollowRequest(request models.Request) (int, error) {
 	var requestID int
 	query := `
 	SELECT 
@@ -36,38 +35,38 @@ func CreateFollowRequest(request models.Request)(int, error){
 		WHERE
 		 (sender_id = $1 AND receiver_id = $2) 
 `
-err := DB.QueryRow(context.Background(),query, request.SenderID, request.ReceiverID).Scan(requestID)
-if err != nil && err.Error() != "no rows in result set" {
-	log.Printf("database: Failed check for request: %v", err)
-	return 0, err // Return error if failed to insert post
-}
-if request.Status == "pending"{
-	log.Printf("request already made")
-	return request.ID, nil 
-}
+	err := DB.QueryRow(context.Background(), query, request.SenderID, request.ReceiverID).Scan(requestID)
+	if err != nil && err.Error() != "no rows in result set" {
+		log.Printf("database: Failed check for request: %v", err)
+		return 0, err // Return error if failed to insert post
+	}
+	if request.Status == "pending" {
+		log.Printf("request already made")
+		return request.ID, nil
+	}
 	query = `
 	INSERT INTO 
 			follow_requests (sender_id, receiver_id) 
 	VALUES 
 			($1, $2)
 	RETURNING request_id`
-err = DB.QueryRow(context.Background(),query, request.SenderID, request.ReceiverID).Scan(&request.ID)
-if err != nil {
-	log.Printf("database: Failed to insert request into database: %v", err)
-	return 0, err // Return error if failed to insert post
-}
-return request.ID, nil
+	err = DB.QueryRow(context.Background(), query, request.SenderID, request.ReceiverID).Scan(&request.ID)
+	if err != nil {
+		log.Printf("database: Failed to insert request into database: %v", err)
+		return 0, err // Return error if failed to insert post
+	}
+	return request.ID, nil
 }
 
 func RespondToFollow(response models.Response) error {
 	log.Print(response)
 	if response.Status == "accepted" {
 		query := `UPDATE follow_requests SET status = $1 WHERE sender_id = $2 AND receiver_id = $3 RETURNING request_id`
-		err := DB.QueryRow(context.Background(),query,"accepted", response.FollowerID, response.FolloweeID).Scan(&response.ID)
+		err := DB.QueryRow(context.Background(), query, "accepted", response.FollowerID, response.FolloweeID).Scan(&response.ID)
 		if err != nil {
 			log.Printf("database: Failed to update response in database: %v", err)
 			return err // Return error if failed to insert post
-		} 
+		}
 		if response.ID == 0 {
 			log.Print("no match")
 			return nil
@@ -76,18 +75,32 @@ func RespondToFollow(response models.Response) error {
 			follower (follower_id, followed_id) 
 	VALUES 
 			($1, $2)`
-	_, err = DB.Exec(context.Background(),query, response.FollowerID, response.FolloweeID)
-			if err != nil {
-				log.Printf("database: Failed to add follower: %v", err)
-				return err // Return error if failed to insert post
-			} 
-	} else if response.Status == "rejected"{
+		_, err = DB.Exec(context.Background(), query, response.FollowerID, response.FolloweeID)
+		if err != nil {
+			log.Printf("database: Failed to add follower: %v", err)
+			return err // Return error if failed to insert post
+		}
+	} else if response.Status == "rejected" {
 		query := `UPDATE follow_requests SET status = 'rejected' WHERE sender_id = $1 AND receiver_id = $2`
-		_, err := DB.Exec(context.Background(),query, response.FollowerID, response.FolloweeID)
+		_, err := DB.Exec(context.Background(), query, response.FollowerID, response.FolloweeID)
 		if err != nil {
 			log.Printf("database: Failed to update response in database: %v", err)
 			return err // Return error if failed to insert post
-		} 
+		}
 	}
 	return nil
+}
+
+// IsFollowing checks if a user is following another user
+func IsFollowing(userID string, followedID string) (bool) {
+	query := `SELECT followed_id FROM follower WHERE follower_id = $1 AND followed_id = $2`
+	err := DB.QueryRow(context.Background(), query, userID, followedID).Scan(&followedID)
+	if err != nil {
+		if err.Error() == "no rows in result set" {
+			return false
+		}
+		log.Printf("database: Failed to check if user is following: %v", err)
+		return false
+	}
+	return true
 }

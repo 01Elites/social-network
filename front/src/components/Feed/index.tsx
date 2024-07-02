21fe623693aa767bfd3d022045343b0699b22193
ExTBH <natheer@extbh.dev>
2024-07-03 00:52:44 +0300

    front(home): add gap

--- front/src/components/Feed/index.tsx
+++ front/src/components/Feed/index.tsx
@@ -8,7 +8,7 @@ interface FeedProps {
 
 export default function Feed(props: FeedProps): JSXElement {
   return (
-    <section class={cn('flex flex-col', props.class)}>
+    <section class={cn('flex flex-col gap-3', props.class)}>
       <h1 class='text-xl font-bold'>Feed</h1>
       <NewPostCell />
     </section>

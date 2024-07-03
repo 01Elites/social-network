46b01043a0a848f7788326d896d51466df211d4e
ExTBH <natheer@extbh.dev>
2024-07-03 19:21:34 +0300

    front(auth): custom authed fetch

---
+++ front/src/extensions/fetch.ts
@@ -0,0 +1,29 @@
+/// <reference lib="dom" />
+
+export async function fetchWithAuth(
+  input: RequestInfo | URL,
+  init?: RequestInit,
+): Promise<Response> {
+  const token = localStorage.getItem('SN_TOKEN') || '';
+
+  // Add the Authorization header to the request
+  const options: RequestInit = init || {};
+
+  if (token !== '') {
+    options.headers = {
+      ...options.headers,
+      Authorization: `Bearer ${token}`,
+    };
+  }
+
+  // Perform the fetch
+  return fetch(input, init).then((response) => {
+    // Check for new token in the response headers
+    const newToken = response.headers.get('Set-Authorization');
+    if (newToken) {
+      localStorage.setItem('SN_TOKEN', newToken.replace('Bearer ', ''));
+    }
+
+    return response;
+  });
+}

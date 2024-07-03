44946c9b73781a50cb4536a9bd51ed662e92b84c
ExTBH <natheer@extbh.dev>
2024-07-04 00:57:37 +0300

    front(new post): working on new post popup

---
+++ front/src/components/Feed/NewPostPreview.tsx
@@ -0,0 +1,36 @@
+import { JSXElement } from 'solid-js';
+import { Button } from '~/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '~/components/ui/dialog';
+import { Separator } from '~/components/ui/separator';
+
+interface NewPostPreviewProps {
+  body?: string;
+}
+
+export default function NewPostPreview(props: NewPostPreviewProps): JSXElement {
+  return (
+    <Dialog defaultOpen={false}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Apparently he is Disappointed</DialogTitle>
+          <DialogDescription>
+            It's okay <b>clown</b> 🤡 don't cry yet. Wait till you read the
+            comments
+          </DialogDescription>
+        </DialogHeader>
+        <Separator />
+        <DialogFooter>
+          <Button variant={'secondary'}>Cancel</Button>
+          <Button>Post</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- front/src/components/Feed/index.tsx
+++ front/src/components/Feed/index.tsx
@@ -1,6 +1,7 @@
 import { JSXElement } from 'solid-js';
 import { cn } from '~/lib/utils';
 import NewPostCell from './NewPostCell';
+import NewPostPreview from './NewPostPreview';
 
 interface FeedProps {
   class?: string;
@@ -11,6 +12,7 @@ export default function Feed(props: FeedProps): JSXElement {
     <section class={cn('flex flex-col gap-3', props.class)}>
       <h1 class='text-xl font-bold'>Feed</h1>
       <NewPostCell />
+      <NewPostPreview />
     </section>
   );
 }

e88b3f7be497388a141172385c6d33f875e3e62c
Mohammed AlSammak <msk170a@gmail.com>
2024-07-04 19:38:05 +0300

    sample image and aligning

--- front/src/components/Feed/PostsCards.tsx
+++ front/src/components/Feed/PostsCards.tsx
@@ -7,19 +7,24 @@ import {
   // CardHeader,
   // CardTitle
 } from "~/components/ui/card"
+import sample_img from "../../assets/sample.avif"
 
 export default function PostsCards(): JSXElement {
   return (
     <>
       <Card>
-        <CardContent>
-          <p>Image</p>
-          <p>Author</p>
-          <p>Text</p>
-        </CardContent>
-        <CardFooter>
-          <p>Likes and comments</p>
-        </CardFooter>
+        <div class="flex items-center justify-center flex-col bg-green">
+          <CardContent>
+            <img src={sample_img} alt="Example Image" width={400} class="flex align-center text-center" />
+            <p class="border">Author</p>
+            <p class="border">Text</p>
+
+          </CardContent>
+          <CardFooter>
+            <p>Likes and comments</p>
+          </CardFooter>
+        </div>
+
       </Card>
     </>
   );

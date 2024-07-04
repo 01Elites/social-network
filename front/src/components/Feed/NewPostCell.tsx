48a250215be593921102ca725ec3fde3fbbd56e1
ExTBH <natheer@extbh.dev>
2024-07-05 01:32:32 +0300

    front(): i forgot

--- front/src/components/Feed/NewPostCell.tsx
+++ front/src/components/Feed/NewPostCell.tsx
@@ -1,5 +1,4 @@
-import { JSXElement, createSignal, useContext } from 'solid-js';
-import { TextField, TextFieldInput } from '~/components/ui/text-field';
+import { JSXElement, useContext } from 'solid-js';
 import UserDetailsContext from '~/contexts/UserDetailsContext';
 import { UserDetailsHook } from '~/types/User';
 
@@ -10,30 +9,24 @@ import { Button } from '~/components/ui/button';
 export default function NewPostCell(): JSXElement {
   const { userDetails } = useContext(UserDetailsContext) as UserDetailsHook;
 
-  const [postText, setPostText] = createSignal('');
-
   return (
     <div class='flex gap-2 rounded border-[1px] p-2'>
+      {/* <NewPostPreview /> */}
+
       <Avatar>
         <AvatarImage src={userDetails()?.avatar_url} />
         <AvatarFallback>
           {userDetails()?.first_name.charAt(0).toUpperCase()}
         </AvatarFallback>
       </Avatar>
-
-      <TextField class='grow' onChange={setPostText}>
-        <TextFieldInput
-          class='border-none'
-          type='text'
-          placeholder='Your father disappointed?'
-        />
-      </TextField>
-      {postText().trim().length > 0 && (
-        <Button class='animate-content-show3' title='Post Button'>
-          Post
-        </Button>
-      )}
-      <Button variant='ghost' title='Upload an image Button'>
+      <Button variant='ghost' class='w-full justify-start'>
+        Your father disappointed?
+      </Button>
+      <Button
+        variant='ghost'
+        title='Upload an image Button'
+        class='hidden xs:inline-flex'
+      >
         <img src={photo} alt='Upload an image icon' />
       </Button>
     </div>

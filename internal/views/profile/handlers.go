71dc3e0fe2f043189f50fbd2e6b7632c81fcc7db
emahfoodh <eman.mjm@gmail.com>
2024-07-07 17:15:48 +0300

    log errors

--- internal/views/profile/handlers.go
+++ internal/views/profile/handlers.go
@@ -33,7 +33,7 @@ func getProfile(w http.ResponseWriter, r *http.Request) {
 	// Retrieve the userID from context using the same key defined globally
 	userID, ok := r.Context().Value(middleware.UserIDKey).(string)
 	if !ok {
-		log.Printf("User ID not found")
+		log.Printf("User ID not found in get profile handler\n")
 		helpers.HTTPError(w, "User ID not found", http.StatusInternalServerError)
 		return
 	}
@@ -41,13 +41,13 @@ func getProfile(w http.ResponseWriter, r *http.Request) {
 	var profile profileData
 	user, err := database.GetUserByID(userID)
 	if err != nil {
-		log.Printf("Failed to get user details: %v", err)
+		log.Printf("Failed to get user details: %v\n", err)
 		helpers.HTTPError(w, "Failed to get user details", http.StatusInternalServerError)
 		return
 	}
 	prof, err := database.GetUserProfile(userID)
 	if err != nil {
-		log.Printf("Error getting user profile: %v", err)
+		log.Printf("Error getting user profile: %v\n", err)
 		helpers.HTTPError(w, "Failed to get user profile", http.StatusInternalServerError)
 		return
 	}
@@ -71,7 +71,7 @@ func getProfile(w http.ResponseWriter, r *http.Request) {
 		ProfilePrivacy: prof.ProfilePrivacy,
 	}
 	if err := json.NewEncoder(w).Encode(profile); err != nil {
-		log.Printf("Failed to encode profile data: %v", err)
+		log.Printf("Failed to encode profile data: %v\n", err)
 		helpers.HTTPError(w, "Something Went Wrong!!", http.StatusInternalServerError)
 		return
 	}
@@ -81,7 +81,7 @@ func patchProfile(w http.ResponseWriter, r *http.Request) {
 	// Retrieve the userID from context using the same key defined globally
 	userID, ok := r.Context().Value(middleware.UserIDKey).(string)
 	if !ok {
-		log.Printf("User ID not found")
+		log.Printf("User ID not found in patch profile handler\n")
 		helpers.HTTPError(w, "Something Went Wrong!!", http.StatusInternalServerError)
 		return
 	}
@@ -89,7 +89,7 @@ func patchProfile(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
 	err := decoder.Decode(&update)
 	if err != nil {
-		log.Printf("Error decoding request body: %v", err)
+		log.Printf("Error decoding request body: %v\n", err)
 		helpers.HTTPError(w, "Something Went Wrong!!", http.StatusBadRequest)
 		return
 	}
@@ -114,14 +114,14 @@ func patchProfile(w http.ResponseWriter, r *http.Request) {
 	}
 	// Validate the profile data
 	if err := helpers.ValidateUserProfileData(&updatedProfile); err != nil {
-		log.Printf("Validation error: %v", err)
+		log.Printf("Validation error: %v\n", err)
 		helpers.HTTPError(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	// Update the user profile
 	if err := database.UpdateUserProfile(updatedProfile); err != nil {
-		log.Printf("Failed to update user profile: %v", err)
+		log.Printf("Failed to update user profile: %v\n", err)
 		helpers.HTTPError(w, "Something Went Wrong!!", http.StatusInternalServerError)
 		return
 	}
@@ -142,7 +142,7 @@ func getProfileByUserName(w http.ResponseWriter, r *http.Request) {
 	userName := r.PathValue("user_name")
 	UserPageID, err := database.GetUserIDByUserName(userName)
 	if err != nil {
-		log.Printf("Failed to get userID: %v", err)
+		log.Printf("Failed to get userID: %v\n", err)
 		helpers.HTTPError(w, "Failed to get userID", http.StatusInternalServerError)
 		return
 	}
@@ -150,13 +150,13 @@ func getProfileByUserName(w http.ResponseWriter, r *http.Request) {
 	var profile profileData
 	user, err := database.GetUserByID(UserPageID)
 	if err != nil {
-		log.Printf("Failed to get user: %v", err)
+		log.Printf("Failed to get user: %v\n", err)
 		helpers.HTTPError(w, "Failed to get user", http.StatusInternalServerError)
 		return
 	}
 	prof, err := database.GetUserProfile(UserPageID)
 	if err != nil {
-		log.Printf("Error getting user profile: %v", err)
+		log.Printf("Error getting user profile: %v\n", err)
 		helpers.HTTPError(w, "Failed to get user profile", http.StatusInternalServerError)
 		return
 	}
@@ -181,7 +181,7 @@ func getProfileByUserName(w http.ResponseWriter, r *http.Request) {
 		Follow_status:  database.GetFollowStatus(userID, UserPageID),
 	}
 	if err := json.NewEncoder(w).Encode(profile); err != nil {
-		log.Printf("Failed to encode profile data: %v", err)
+		log.Printf("Failed to encode profile data: %v\n", err)
 		helpers.HTTPError(w, "Something Went Wrong!!", http.StatusInternalServerError)
 		return
 	}
@@ -190,7 +190,7 @@ func getProfileByUserName(w http.ResponseWriter, r *http.Request) {
 func getProfilePosts(w http.ResponseWriter, r *http.Request) {
 	userID, ok := r.Context().Value(middleware.UserIDKey).(string)
 	if !ok {
-		log.Printf("User ID not found")
+		log.Printf("User ID not found in get profile posts handler\n")
 		http.Error(w, "User ID not found", http.StatusInternalServerError)
 		return
 	}
@@ -198,14 +198,14 @@ func getProfilePosts(w http.ResponseWriter, r *http.Request) {
 	userName := r.PathValue("user_name")
 	UserPageID, err := database.GetUserIDByUserName(userName)
 	if err != nil {
-		log.Printf("Failed to get userID: %v", err)
+		log.Printf("Failed to get userID: %v\n", err)
 		helpers.HTTPError(w, "Failed to get userID", http.StatusInternalServerError)
 		return
 	}
 
 	posts, err := database.GetUserPosts(userID, UserPageID, database.IsFollowing(userID, UserPageID))
 	if err != nil {
-		log.Printf("Failed to get posts: %v", err)
+		log.Printf("Failed to get posts: %v\n", err)
 		helpers.HTTPError(w, "Failed to get posts", http.StatusNotFound)
 		return
 	}

0db68e7e19de529aecc605bf6fee5c69bd015ab2
aalali <ahmedalali5294@gmail.com>
2024-07-08 03:38:39 +0300

    changed like api request

--- internal/views/post/likeshandler.go
+++ internal/views/post/likeshandler.go
@@ -3,10 +3,10 @@ package post
 import (
 	"encoding/json"
 	"net/http"
-	"strconv"
 
 	database "social-network/internal/database/querys"
 	"social-network/internal/helpers"
+	"social-network/internal/models"
 	"social-network/internal/views/middleware"
 )
 
@@ -17,25 +17,25 @@ func CreateLikeHandler(w http.ResponseWriter, r *http.Request) {
 		helpers.HTTPError(w, "User ID not found", http.StatusInternalServerError)
 		return
 	}
-	post_id, err := strconv.Atoi(r.PathValue("id"))
+	var post models.ID
+	err := json.NewDecoder(r.Body).Decode(&post)
 	if err != nil {
-		helpers.HTTPError(w, ("Invalid postID:" + err.Error()), http.StatusBadRequest)
+		helpers.HTTPError(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	// chaek if the post_id exists
-	count, err := database.GetPostCountByID(post_id)
+	count, err := database.GetPostCountByID(post.ID)
 	if err != nil || count == 0 {
 		helpers.HTTPError(w, ("Post not found" + err.Error()), http.StatusBadRequest)
 		return
 	}
 
 	// Update the like in the database
-	err = database.UpDateLikeInDB(userID, post_id)
+	err = database.UpDateLikeInDB(userID, post.ID)
 	if err != nil {
 		helpers.HTTPError(w, ("Failed to create like:" + err.Error()), http.StatusInternalServerError)
 		return
 	}
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(post_id)
+	w.WriteHeader(http.StatusOK)
 }

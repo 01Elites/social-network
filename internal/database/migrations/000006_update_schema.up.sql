d0df67fa378e48e4e4f33f54a9b0fdda84ae223f
emahfoodh <eman.mjm@gmail.com>
2024-07-08 17:31:47 +0300

    revert no need to remove column

--- internal/database/migrations/000006_update_schema.up.sql
+++ internal/database/migrations/000006_update_schema.up.sql
@@ -2,9 +2,3 @@
 ALTER TABLE public.post_user
 DROP CONSTRAINT IF EXISTS post_user_post_id_fkey,
 ADD CONSTRAINT post_user_post_id_fkey FOREIGN KEY (post_id) REFERENCES public.post(post_id) ON DELETE CASCADE;
-
--- Drop the interaction_type column and enum type
-ALTER TABLE public.post_interaction
-DROP COLUMN interaction_type;
-
-DROP TYPE public.interaction_type;

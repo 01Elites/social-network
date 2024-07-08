8bd41bf30d8aef47546a8dfae6592ea58ef5bbd6
emahfoodh <eman.mjm@gmail.com>
2024-07-08 19:18:10 +0300

    update documentation

--- internal/views/post/comments_handler.go
+++ internal/views/post/comments_handler.go
@@ -5,11 +5,12 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strconv"
+
 	database "social-network/internal/database/querys"
 	"social-network/internal/helpers"
 	"social-network/internal/models"
 	"social-network/internal/views/middleware"
-	"strconv"
 )
 
 /*
@@ -20,12 +21,13 @@ It requires a valid user session to create a comment.
 
 Example:
 
-	    // To create a new comment on a post
-	    POST /api/post/{id}/comments
-{
-    "body": "string",
-    "image_id": "string" // optional
-}
+	// To create a new comment on a post
+	POST /api/post/{id}/comments
+
+	{
+	    "body": string,
+	    "image_id": string // optional
+	}
 */
 func CreateCommentHandler(w http.ResponseWriter, r *http.Request) {
 	userID, ok := r.Context().Value(middleware.UserIDKey).(string)
@@ -36,13 +38,15 @@ func CreateCommentHandler(w http.ResponseWriter, r *http.Request) {
 	var comment models.Create_Comment
 	parentID, err := strconv.Atoi(r.PathValue("id"))
 	if err != nil {
-		helpers.HTTPError(w, ("Failed to decode post id" + err.Error()), http.StatusBadRequest)
+		log.Println("Failed to convert post ID to int:", err)
+		helpers.HTTPError(w, "Invalid post ID:", http.StatusBadRequest)
 		return
 	}
 	comment.ParentID = parentID
 	err = json.NewDecoder(r.Body).Decode(&comment)
 	if err != nil {
-		helpers.HTTPError(w, ("Failed to decode comment:" + err.Error()), http.StatusBadRequest)
+		log.Println("Failed to decode comment:", err)
+		helpers.HTTPError(w, "Failed to decode comment:", http.StatusBadRequest)
 		return
 	}
 	if comment.Image != "" && comment.Image != "null" {
@@ -54,13 +58,13 @@ func CreateCommentHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	err = database.Create_Comment_in_db(userID, comment)
 	if err != nil {
-		helpers.HTTPError(w, ("Invalid post ID:" + err.Error()), http.StatusNotFound)
+		log.Println("Failed to create comment:", err)
+		helpers.HTTPError(w, "Invalid post ID:", http.StatusNotFound)
 		return
 	}
 	w.WriteHeader(http.StatusCreated)
 	io.WriteString(w, "create comment successful")
 }
-
 
 /*
 GetPostCommentsHandler retrieves comments for a specific post.
@@ -75,17 +79,20 @@ Example:
 
 Response:
 
-	[
-    {
-        "body": "string",
-        "image_id": "string" // optional 
-        "commenter": {
-            "first_name": "string",
-            "last_name": "string",
-            "image_id": "string",
-            "user_name":"string"
-        }
-    }
+		[
+	    {
+	        "comment_id": int,
+	        "image_id": string // optional
+					"body": string,
+					"creation_date": "2024-07-08T16:10:12.595563Z", // unix timestamp
+	        "commenter": {
+	            "first_name": string,
+	            "last_name": string,
+	            "image_id": string, //optional
+	            "user_name":string
+	        }
+	    }
+
 ]
 */
 func GetPostCommentsHandler(w http.ResponseWriter, r *http.Request) {

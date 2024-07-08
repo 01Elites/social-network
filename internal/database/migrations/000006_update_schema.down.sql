a81718005836dbb7568884597bf6c78d3cdf2ed3
emahfoodh <eman.mjm@gmail.com>
2024-07-08 17:21:59 +0300

    no interaction type

--- internal/database/migrations/000006_update_schema.down.sql
+++ internal/database/migrations/000006_update_schema.down.sql
@@ -2,3 +2,9 @@
 ALTER TABLE public.post_user
 DROP CONSTRAINT IF EXISTS post_user_post_id_fkey,
 ADD CONSTRAINT post_user_post_id_fkey FOREIGN KEY (post_id) REFERENCES public.post(post_id);
+
+-- Recreate the interaction_type enum type and add the column back
+CREATE TYPE public.interaction_type AS ENUM ('like', 'dislike');
+
+ALTER TABLE public.post_interaction
+ADD COLUMN interaction_type public.interaction_type NOT NULL;

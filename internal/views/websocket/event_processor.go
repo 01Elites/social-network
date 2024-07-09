bcf27d25d7a8e875bf938a13721150efee1d0a9f
amali <e.amjad2010@gmail.com>
2024-07-09 19:39:04 +0300

    websocket event processing functions and add todo list

--- internal/views/websocket/event_processor.go
+++ internal/views/websocket/event_processor.go
@@ -41,8 +41,40 @@ func ProcessEvents(conn *websocket.Conn, userName string) {
 		case events_type.SEND_MESSAGE:
 			// Call function for NOTIFICATION
 			log.Println("SEND_MESSAGE")
+		case events_type.TYPING:
+			// Call function for TYPING
+			// Typing(event, user)
+		case events_type.CHAT_OPENED:
+			// OpenChat(event, user)
+		case events_type.CHAT_CLOSED:
+			// CloseChat(user)
+		case events_type.GET_MESSAGES:
+			// GetMessages(event, user)
+		case events_type.GET_NOTIFICATIONS:
+			// GetNotifications(event, user)
 		default:
 			log.Println("Unknown event type:", message.Event)
 		}
 	}
 }
+
+/// todo:
+// 1. Implement the Typing function
+// 2. Implement the OpenChat function
+// 3. Implement the CloseChat function
+// 4. Implement the GetMessages function
+// 5. Implement the GetNotifications function
+// 6. get the list of users {online, offline,Dm's } and send it to the client
+// 7. get the list of groups {online, offline, messages} and send it to the client
+
+// to do with the notifications
+// 1. Implement the notification function {send notification to the user by websocket}
+// 2. notifications include:
+// 	- new comment on the my post {post_id, commenter , comment}
+// 	- new like on the my post {post_id, liker}
+// 	- new friend request "follow request" {follower} to be [accepted or rejected]
+// 	- new message in DM {sender, message}
+// 	- new message in the group chat {sender, message , group_id}
+//  - invitation to the group
+//  - new reuest to join the group {which the user is the admin}
+//  - new event in the group {which the user is the admin} MAYBE !!!

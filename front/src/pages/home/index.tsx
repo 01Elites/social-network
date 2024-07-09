fecb2782dfda1818a1e9495efa1d39767390ac1a
ExTBH <natheer@extbh.dev>
2024-07-10 00:09:07 +0300

    front(home): add events section

--- front/src/pages/home/index.tsx
+++ front/src/pages/home/index.tsx
@@ -7,8 +7,11 @@ export default function HomePage(): JSXElement {
   return (
     <Layout>
       <section class='flex h-full gap-4'>
-        <Feed class='w-3/4' />
-        <div class='grow'>
+        <div class='w-1/3 max-w-60'>
+          <h1 class='text-xl font-bold'>Events</h1>
+        </div>
+        <Feed class='grow' />
+        <div class='w-1/3 max-w-52'>
           <h1 class='text-xl font-bold'>Contacts</h1>
         </div>
       </section>

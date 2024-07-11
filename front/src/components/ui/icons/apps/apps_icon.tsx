4ec4cddc9b4e5308edd78a8e34f6cfa6dd1e16dd
Mohammed AlSammak <msk170a@gmail.com>
2024-07-12 00:11:26 +0300

    icons dark-mode fix

--- front/src/components/ui/icons/apps/apps_icon.tsx
+++ front/src/components/ui/icons/apps/apps_icon.tsx
@@ -1,5 +1,4 @@
 import { JSXElement } from 'solid-js';
-import apps_icon from './apps.svg';
 
 interface inputs {
   class?: string;
@@ -8,13 +7,15 @@ interface inputs {
 
 export default function Apps_Icon(props?: inputs): JSXElement {
   return (
-    <div class='flex h-11 w-11 items-center justify-center rounded bg-accent'>
-      <img
-        src={apps_icon}
-        alt='Apps'
-        class={props ? props.class : ''}
-        onClick={props ? props.onClick : () => {}}
-      />
+    <div class='flex h-11 w-11 items-center justify-center rounded bg-accent' onClick={props?.onClick}>
+      <div class={props?.class}>
+        <svg style={{ width: "100%", height: "100%" }} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <rect x="2.5" y="2.5" width="8" height="8" stroke="hsl(var(--primary))" />
+          <rect x="2.5" y="13.5" width="8" height="8" stroke="hsl(var(--primary))" />
+          <rect x="13.5" y="2.5" width="8" height="8" stroke="hsl(var(--primary))" />
+          <rect x="13.5" y="13.5" width="8" height="8" stroke="hsl(var(--primary))" />
+        </svg>
+      </div>
     </div>
   );
 }
--- front/src/components/ui/icons/flag/flag_icon.tsx
+++ front/src/components/ui/icons/flag/flag_icon.tsx
@@ -1,5 +1,4 @@
 import { JSXElement } from 'solid-js';
-import flag_icon from './flag.svg';
 
 interface inputs {
   class?: string;
@@ -8,11 +7,18 @@ interface inputs {
 
 export default function Flag_Icon(props?: inputs): JSXElement {
   return (
-    <img
-      src={flag_icon}
-      alt='Apps'
-      class={props ? props.class : ''}
-      onClick={props ? props.onClick : () => {}}
-    />
+    <div class={props?.class} onClick={props?.onClick}>
+      <svg style={{ width: "100%", height: "100%" }} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g clip-path="url(#clip0_1_2)">
+          <path d="M2.25 2.2615L20.8254 11L2.25 19.7385L2.25 2.2615Z" stroke="hsl(var(--primary))" />
+          <line x1="2.5" y1="2" x2="2.5" y2="22" stroke="hsl(var(--primary))" />
+        </g>
+        <defs>
+          <clipPath id="clip0_1_2">
+            <rect width="20" height="20" fill="white" transform="translate(2 2)" />
+          </clipPath>
+        </defs>
+      </svg>
+    </div>
   );
 }

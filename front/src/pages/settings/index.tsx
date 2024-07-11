36e0bca7d5040ff73001fc8898415cd14b9c288e
ExTBH <natheer@extbh.dev>
2024-07-11 18:06:45 +0300

    front(): add darkmode

--- front/src/pages/settings/index.tsx
+++ front/src/pages/settings/index.tsx
@@ -1,10 +1,13 @@
+import { useColorMode } from '@kobalte/core/color-mode';
 import { createSignal, JSXElement } from 'solid-js';
+import { Label } from '~/components/ui/label';
 import {
   Sheet,
   SheetContent,
   SheetHeader,
   SheetTitle,
 } from '~/components/ui/sheet';
+import { Switch, SwitchControl, SwitchThumb } from '~/components/ui/switch';
 
 /**
  * Opens the settings sheet.
@@ -24,12 +27,36 @@ function showSettings() {
  * @returns JSXElement representing the SettingsPage component.
  */
 function SettingsPage(): JSXElement {
+  const { colorMode, setColorMode } = useColorMode();
+
   return (
     <Sheet open={settingsOpen()} onOpenChange={setSettingsOpen}>
       <SheetContent position='left'>
-        <SheetHeader>
+        <SheetHeader class='mb-4'>
           <SheetTitle>Settings</SheetTitle>
         </SheetHeader>
+        <h3 class='text-base font-semibold'>Theme</h3>
+        <section>
+          <div class='flex items-center justify-between'>
+            <Label for='dark-swtich'>Dark Mode</Label>
+            <Switch
+              checked={colorMode() === 'dark'}
+              onChange={(isChecked) => {
+                if (isChecked) {
+                  setColorMode('dark');
+                } else {
+                  setColorMode('light');
+                }
+              }}
+            >
+              <SwitchControl>
+                <SwitchThumb />
+              </SwitchControl>
+            </Switch>
+          </div>
+        </section>
+
+        {/* <Separator /> */}
       </SheetContent>
     </Sheet>
   );

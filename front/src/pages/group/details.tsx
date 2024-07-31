982ac6c8d94fcbc61b6f67b5f819c099a8684aff
AhmedAlAli9402 <ahmedalali5294@gmail.com>
2024-07-31 16:43:50 +0300

    fixed error with display

--- front/src/pages/group/details.tsx
+++ front/src/pages/group/details.tsx
@@ -11,23 +11,26 @@ export default function GroupDetails(props: { targetGroup: () => Group}): JSXEle
   const numberOfMembers = props.targetGroup().members.length;
   const groupID = String(props.targetGroup().id)
   return (
-    <div class='flex flex-col'> {/* Left div */}
+    <div class='flex flex-col w-[30rem]'> {/* Left div */}
       <div class='flex flex-col justify-center items-center'>
-        <Show when={props.targetGroup().creator.avatar}>
         <AspectRatio ratio={16 / 9}>
           <div class='absolute inset-0 bg-black bg-opacity-50 flex justify-center items-end rounded-lg'>
-            <Avatar class='w-[5rem] h-[5rem] mb-2'>
+          <Avatar class='w-[5rem] h-[5rem] mb-2'>
               <AvatarFallback>
-                  {props.targetGroup().title.charAt(0).toUpperCase()}
-</AvatarFallback>
+                <Show when={props.targetGroup().creator.avatar} fallback={
+                  props.targetGroup().title.charAt(0).toUpperCase()
+                }><img
+                    class='size-full rounded-md rounded-b-none object-cover'
+                    loading='lazy'
+                    src={`${config.API_URL}/image/${props.targetGroup().creator.avatar}`}
+                  /></Show></AvatarFallback>
             </Avatar>
-          </div>
+            </div>
         </AspectRatio>
-      </Show>
          {/* Profile picture */}
         <div class='flex flex-col items-center w-full'> {/* Username, followers, following */}
-          <p class='text-2xl font-bold m-2'>{props.targetGroup().title}</p>
-          <div class='grid w-full grid-cols-2 text-sm m-2'>
+          <p class='text-2xl font-bold m-4'>{props.targetGroup().title}</p>
+          <div class='grid w-full grid-cols-2 text-sm m-4'>
           <p class='flex justify-center'>Group Creator:&nbsp<A
               href={"/profile/" + props.targetGroup().creator.user_name} class='block text-sm font-bold hover:underline'>
           {props.targetGroup().creator.first_name}  {props.targetGroup().creator.last_name}</A></p>

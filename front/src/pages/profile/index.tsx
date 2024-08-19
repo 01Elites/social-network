2a6c0e6404cd7613d7c2a2da09678e42de0c47ab
Mohammed AlSammak <msk170a@gmail.com>
2024-08-19 18:44:18 +0300

    changing Show to Switch

--- front/src/pages/profile/index.tsx
+++ front/src/pages/profile/index.tsx
@@ -3,7 +3,9 @@ import {
   createEffect,
   createSignal,
   JSXElement,
+  Match,
   Show,
+  Switch,
   useContext,
 } from 'solid-js';
 import config from '~/config';
@@ -14,6 +16,7 @@ import Layout from '~/Layout';
 import User from '~/types/User';
 import ProfileFeed from './proFeed';
 import ProfileDetails from './profileDetails';
+import ErrPage from '../404';
 
 type ProfileParams = {
   username: string;
@@ -22,6 +25,7 @@ type ProfileParams = {
 export default function Profile(): JSXElement {
   const userCtx = useContext(UserDetailsContext) as UserDetailsHook;
   const [targetUser, setTargetUser] = createSignal<User | undefined>();
+  const [error, setError] = createSignal<boolean>(false);
 
   const params: ProfileParams = useParams();
 
@@ -40,8 +44,9 @@ export default function Profile(): JSXElement {
     fetchWithAuth(config.API_URL + '/profile/' + params.username).then(
       async (res) => {
         const body = await res.json();
-        if (res.status === 404) {
+        if (res.status === 500) {
           console.log('User not found');
+          setError(true)
           return;
         }
         if (res.ok) {
@@ -55,14 +60,21 @@ export default function Profile(): JSXElement {
   return (
     <Layout>
       <div class='m-4 grid grid-cols-1 md:grid-cols-6'>
-        <div class='col-span-2'>
-          <Show when={targetUser()}>
-            <ProfileDetails targetUser={() => targetUser() as User} />
-          </Show>
-        </div>
-        <div class='col-span-4 overflow-y-auto'>
-          <ProfileFeed targetUser={() => targetUser() as User} />
-        </div>
+
+        <Switch fallback={<h1>Loading...</h1>}>
+          <Match when={error() == true}>
+            <h1>Bad Request!</h1>
+          </Match>
+          <Match when={targetUser()}>
+            <div class='col-span-2'>
+              <ProfileDetails targetUser={() => targetUser() as User} />
+            </div>
+            <div class='col-span-4 overflow-y-auto'>
+              <ProfileFeed targetUser={() => targetUser() as User} />
+            </div>
+          </Match>
+        </Switch>
+
       </div>{' '}
       {/* Main grid */}
     </Layout>
